b375e7d60ce4cc6dcfce38f80d82b942ea034e24
hitman4ik85 <hitman4ik85@gmail.com>
2024-12-09 20:57:31 +0200

    Practic ZD

--- Practic/FoodPlacesApp/FoodPlacesApp/obj/Debug/net8.0/FoodPlacesApp.AssemblyInfo.cs
+++ Practic/FoodPlacesApp/FoodPlacesApp/obj/Debug/net8.0/FoodPlacesApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodPlacesApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2489796081882be722b80d0d370c0c2024ea7ac5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8a75097081f044be63b719c9e135bc1cc994f88")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodPlacesApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodPlacesApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
